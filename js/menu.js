e1f1d460bc8df71bff7962c94120e9fb70f59c8e
Gelmes <marco.firsteye@gmail.com>
2019-11-03 07:40:48 -0800

    Cleaned up menySaveAs()

--- js/menu.js
+++ js/menu.js
@@ -8,24 +8,21 @@ var $ = require("jquery");
 
 function menuSaveAs(){
     var fileName = dialog.showOpenDialogSync({ title:"Save As", buttonLabel:"Save", properties: ['openFile', 'promptToCreate'] })[0];
-    console.log(fileName);
+
     var dict = {};
     dict = ta.get();
     var ditc_obj = {};
+    // NOTE: Kind of dumb I have to do this
+    // But copying the dictionary content
+    // to this other dictionary converts it to an object
+    // this allows it to work with JSON.stringify()
+    // Not a big performence hit so no problem
     for (var key in dict){
         ditc_obj[key] = dict[key];
     }
-
-    var my_cars= {};
-    my_cars["cool"]="Mustang";
-    my_cars["family"]="Station Wagon";
-    my_cars["big"]="SUV";
-    console.log(ditc_obj);
-    // console.log(my_cars);
     
     // console.log(dict);
     var content = JSON.stringify(ditc_obj, null, 4);
-    console.log(content);
     try { fs.writeFileSync(fileName, content, 'utf-8'); }
     catch(e) { 
         alert('Failed to save the file !'); 
