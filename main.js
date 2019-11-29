e88d4f91a86610702f4173b2495acbbbbdd0cc6b
Gelmes <marco.firsteye@gmail.com>
2019-11-28 18:31:26 -0800

    Fixed builds icon issues, added install handlers

--- main.js
+++ main.js
@@ -1,8 +1,77 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 // Stop app from launching during installation
 // see: https://www.npmjs.com/package/@electron-forge/maker-squirrel
-if (require('electron-squirrel-startup')) return app.quit();
+if (require('electron-squirrel-startup')) return;
+
+if (handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
+}
+
+function handleSquirrelEvent() {
+  if (process.argv.length === 1) {
+    return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const path = require('path');
+
+  const appFolder = path.resolve(process.execPath, '..');
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const exeName = path.basename(process.execPath);
+
+  const spawn = function(command, args) {
+    let spawnedProcess, error;
+
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
+    } catch (error) {}
+
+    return spawnedProcess;
+  };
+
+  const spawnUpdate = function(args) {
+    return spawn(updateDotExe, args);
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      // Optionally do things such as:
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Install desktop and start menu shortcuts
+      spawnUpdate(['--createShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-uninstall':
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Remove desktop and start menu shortcuts
+      spawnUpdate(['--removeShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-obsolete':
+      // This is called on the outgoing version of your app before
+      // we update to the new version - it's the opposite of
+      // --squirrel-updated
+
+      app.quit();
+      return true;
+  }
+};
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -14,7 +83,7 @@ function createWindow () {
   win = new BrowserWindow({
     width: 1920,
     height: 1060,
-    icon: 'notepad.png',
+    icon: __dirname + 'img/notepad.ico',
     webPreferences: {
       nodeIntegration: true
     },
@@ -25,6 +94,7 @@ function createWindow () {
   // and load the index.html of the app.
   win.loadFile('index.html');
 
+  win.setIcon(path.join(__dirname, 'img/notepad.png'));
   // Open the DevTools.
   win.webContents.openDevTools();
 
