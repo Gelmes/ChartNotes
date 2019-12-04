fb950727e57d08df4ccab7d73e9ff992e5104785
Marco Rubio <mrubio@OPTO22.COM>
2019-12-04 14:26:27 -0800

    commented out onClick set Caret to the end when clicking on empty area

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ console.log("Importing: ", "index.js");
 var ta = new TextArea(".TextArea");
 var sh = new Shortcuts(ta);
 
+// NOTE: Causes cursor to go to the end when you try to click a row and edit something in the middle of the row.
 // Clicking on empty space focuses on the current target row
-$("html").click(function (){
-    ta.setCaretToEnd();
-});
+// $("html").click(function (){
+//     ta.setCaretToEnd();
+// });
 
 console.log("Imported : ", "index.js");
