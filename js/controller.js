a4c6487c96397df9de254be9f493a96d60115e7b
Gelmes <marco.firsteye@gmail.com>
2019-12-28 10:41:04 -0800

    Added base controller used to update all models

---
+++ js/controller.js
@@ -0,0 +1,111 @@
+// Controller
+// The controller updates all the models according to the actions activated
+// This function will allow a single function call to update the TextArea
+// and the Undo history along with any other models that might need to be 
+// updated in the future like charts and flow diagrams
+// The views will be updated with a simple call to update view
+// Its expected that the views will have an instance of the models to
+// automatically update themselves with the models contents
+
+
+// newRow
+// downRow
+// upRow
+// incLevel
+// decLevel
+// delRow
+// bacRow
+// fileNew
+// fileSave
+// fileOpen
+// tagIter
+// downRowShrt
+// upRowShrt
+// zoomIn
+// zoomOut
+// moveRowDown
+// moveRowUp
+// showHistory
+// toogleDeveloper
+
+class Controller{
+    constructor(){
+
+    }
+    
+    newRow(){
+
+    }
+    
+    downRow(){
+
+    }
+    
+    upRow(){
+
+    }
+    
+    incLevel(){
+
+    }
+    
+    decLevel(){
+
+    }
+    
+    delRow(){
+
+    }
+    
+    bacRow(){
+
+    }
+    
+    fileNew(){
+
+    }
+    
+    fileSave(){
+
+    }
+    
+    fileOpen(){
+
+    }
+    
+    tagIter(){
+
+    }
+    
+    downRowShrt(){
+
+    }
+    
+    upRowShrt(){
+
+    }
+    
+    zoomIn(){
+
+    }
+    
+    zoomOut(){
+
+    }
+    
+    moveRowDown(){
+
+    }
+    
+    moveRowUp(){
+
+    }
+    
+    showHistory(){
+
+    }
+    
+    toogleDeveloper(){
+
+    }
+}
