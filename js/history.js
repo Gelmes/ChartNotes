0530e52d7facc321532a4c254837dc10a3246d89
Marco Rubio <mrubio@OPTO22.COM>
2020-01-09 15:53:42 -0800

    Moved contents of undo.js to history.js sence that makes more sence

--- js/history.js
+++ js/history.js
@@ -1,8 +1,50 @@
 class History{
     constructor(){
-        this.history = [];
+        this.historyList = [];
+
+        // NOTE: This action list contains the ID(index) of a given action
+        // additional actions can be added to the end of this list
+        this.actionsList = {
+            "newRow":1,
+            "downRow":2,
+            "upRow":3,
+            "increaseLevel":4,
+            "decreaseLevel":5,
+            "deleteRow":6,
+            "backspaceRow":7,
+            "nextStatus":8,
+            "moveRowDown":9,
+            "moveRowUp":10
+        }
     }
-    record(event){
-        this.history.push(event);
+
+    newRow(){
+    }
+
+    downRow(){
+    }
+
+    upRow(){
+    }
+
+    increaseLevel(){
+    }
+
+    decreaseLevel(){
+    }
+
+    deleteRow(){
+    }
+
+    backspaceRow(){
+    }
+
+    nextStatus(){
+    }
+
+    moveRowDown(){
+    }
+
+    moveRowUp(){
     }
 }
