c23f15b2c730238ef40dd0ce7264d118b80f4613
Marco Rubio <mrubio@OPTO22.COM>
2020-01-09 14:58:35 -0800

    added functions to undu.js to make it compatible with controller

--- js/undo.js
+++ js/undo.js
@@ -1,5 +1,50 @@
 class Undo{
     constructor(){
-        this.inverseList = {};
+        this.historyList = [];
+
+        // NOTE: This action list contains the ID(index) of a given action
+        // additional actions can be added to the end of this list
+        this.actionsList = {
+            "newRow":1,
+            "downRow":2,
+            "upRow":3,
+            "increaseLevel":4,
+            "decreaseLevel":5,
+            "deleteRow":6,
+            "nextStatus":7,
+            "moveRowDown":8,
+            "moveRowUp":9
+
+        }
+    }
+
+    newRow(){
+    }
+
+    downRow(){
+    }
+
+    upRow(){
+    }
+
+    increaseLevel(){
+    }
+
+    decreaseLevel(){
+    }
+
+    deleteRow(){
+    }
+
+    deleteRowIfCursorAtStart(){
+    }
+
+    nextStatus(){
+    }
+
+    moveRowDown(){
+    }
+
+    moveRowUp(){
     }
 }
